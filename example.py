49fda9ccb6505d8fdf869575e5b85c95c9ad9f44
Nate Larsen <nlars101@byu.edu>
2026-02-17 22:35:13 -0700

    Toy around with leverage scores

---
+++ example.py
@@ -0,0 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# x = np.sort(np.random.rand(100))
+# y = 2 * x + 1 + np.random.randn(100)
+
+# y[2] = 100  # Add an outlier
+
+# # Regression
+# X = np.vstack([x, np.ones_like(x)]).T
+# beta = np.linalg.lstsq(X, y, rcond=None)[0]
+# plt.scatter(x, y, label="Data")
+# plt.plot(x, X @ beta, color="red", label="Fit")
+# plt.plot(x[2], y[2], "ro", label="Outlier", color="orange")
+# plt.legend()
+# plt.savefig("regression.png")
+
+
+def leverage_scores(X):
+    # Q, _ = np.linalg.qr(X)
+    # return np.sum(Q**2, axis=1)
+    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    return np.sum(U**2, axis=1)
+
+
+# plt.figure()
+# scores = leverage_scores(X)
+# plt.plot(x, scores, label="Leverage Scores")
+# plt.plot(x[2], scores[2], "ro", label="Outlier")
+# plt.legend()
+# plt.savefig("leverage_scores.png")
+
+
+X = np.array(
+    [
+        [1, 0],
+        [0, 1],
+        [1, 10],
+    ]
+)
+
+print(leverage_scores(X))
